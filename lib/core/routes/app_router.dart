2347fa65f5119bf221310b9df93c107c74d0c53b
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-07-07 04:00:22 +0300

    Add splash screen and routing setup for initial app navigation

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,22 @@
+import 'routes.dart';
+import 'package:flutter/material.dart';
+import 'package:soulsync/features/splash/splash_screen.dart';
+
+class AppRouter {
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashScreen:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder:
+              (_) => Scaffold(
+                body: Center(
+                  child: Text('No route defined for ${settings.name}'),
+                ),
+              ),
+        );
+    }
+  }
+}
