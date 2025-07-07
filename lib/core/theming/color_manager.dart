236b1ac4a2a75a17f8d9cea95c6d87bf9fd93f67
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-07-07 03:13:14 +0300

    add the files

---
+++ lib/core/theming/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color teal = Color(0xFF7C9A92);
+  static const Color darkSlate = Color(0xFF253334);
+  static const Color lightBlue = Color(0xFF95CBCF);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGrey = Color(0xFFBEC2C2);
+}
---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:soulsync/core/theming/color_manager.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:soulsync/core/theming/font_weight_helper.dart';
+
+class TextStyles {
+  static TextStyle whiteAlegreya40Bold = GoogleFonts.alegreya(
+    fontSize: 40.sp,
+    color: ColorManager.white,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static TextStyle whiteAlegreyaSans20Medium = GoogleFonts.alegreyaSans(
+    fontSize: 20.sp,
+    color: ColorManager.white,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle whiteAlegreyaSans25Medium = GoogleFonts.alegreyaSans(
+    fontSize: 25.sp,
+    color: ColorManager.white,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle whiteAlegreyaSans20Bold = GoogleFonts.alegreyaSans(
+    fontSize: 20.sp,
+    color: ColorManager.white,
+    fontWeight: FontWeightHelper.bold,
+  );
+}
