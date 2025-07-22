8e3faf80d9d1ebcc5e9da1ae8e1af5d13e1d025f
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-07-22 04:04:19 +0300

    feat: add SupabaseHelper class for Supabase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:soulsync/soul_sync.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:soulsync/core/supabase/supabase_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SupabaseHelper.init();
+
   runApp(SoulSync());
 }
