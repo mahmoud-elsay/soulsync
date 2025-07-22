c59fc60b25223418653b734c0b0e248a6f021cc1
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-07-23 01:16:16 +0300

    feat: implement Login functionality with Bloc integration and add Home screen

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -2,9 +2,11 @@ import 'routes.dart';
 import 'package:flutter/material.dart';
 import 'package:soulsync/core/di/di.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:soulsync/features/home/ui/home_screen.dart';
 import 'package:soulsync/features/splash/splash_screen.dart';
 import 'package:soulsync/features/auth/login/ui/login_screen.dart';
 import 'package:soulsync/features/onboarding/onboarding_screen.dart';
+import 'package:soulsync/features/auth/login/logic/login_cubit.dart';
 import 'package:soulsync/features/auth/sign_up/ui/sign_up_screen.dart';
 import 'package:soulsync/features/auth/sign_up/logic/sign_up_cubit.dart';
 
@@ -18,7 +20,13 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => OnboardingScreen());
 
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create: (context) => getIt<LoginCubit>(),
+                child: const LoginScreen(),
+              ),
+        );
 
       case Routes.signUpScreen:
         return MaterialPageRoute(
@@ -28,6 +36,9 @@ class AppRouter {
                 child: const SignUpScreen(),
               ),
         );
+
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
 
       default:
         return MaterialPageRoute(
--- lib/features/auth/login/ui/widgets/login_form.dart
+++ lib/features/auth/login/ui/widgets/login_form.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:soulsync/core/routes/routes.dart';
 import 'package:soulsync/core/helpers/spacing.dart';
+import 'package:soulsync/core/helpers/extension.dart';
 import 'package:soulsync/core/theming/text_styles.dart';
+import 'package:soulsync/core/helpers/app_validation.dart';
 import 'package:soulsync/core/widgets/app_text_button.dart';
 import 'package:soulsync/core/widgets/app_text_form_field.dart';
+import 'package:soulsync/features/auth/login/logic/login_cubit.dart';
+import 'package:soulsync/features/auth/login/logic/login_state.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -13,6 +19,9 @@ class LoginForm extends StatefulWidget {
 
 class _LoginFormState extends State<LoginForm> {
   bool _isObscureText = true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   void _toggleObscureText() {
     setState(() {
@@ -20,40 +29,102 @@ class _LoginFormState extends State<LoginForm> {
     });
   }
 
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      context.read<LoginCubit>().login(
+        email: _emailController.text.trim(),
+        password: _passwordController.text,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        AppTextFormField(hintText: 'Email Address'),
-        verticalSpace(20),
-        AppTextFormField(
-          hintText: 'Password',
-          isObscureText: _isObscureText,
-          suffixIcon: IconButton(
-            icon: Icon(
-              _isObscureText ? Icons.visibility_off : Icons.visibility,
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        state.when(
+          initial: () {},
+          loading: () {},
+          success: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Login successful!'),
+                backgroundColor: Colors.green,
+              ),
+            );
+            
+            context.pushReplacementNamed(Routes.homeScreen);
+          },
+          error: (error) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(error.message),
+                backgroundColor: Colors.red,
+              ),
+            );
+          },
+        );
+      },
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppTextFormField(
+              controller: _emailController,
+              hintText: 'Email Address',
+              keyboardType: TextInputType.emailAddress,
+              validator: validateEmail,
             ),
-            color: Colors.grey,
-            onPressed: _toggleObscureText,
-          ),
+            verticalSpace(20),
+            AppTextFormField(
+              controller: _passwordController,
+              hintText: 'Password',
+              isObscureText: _isObscureText,
+              validator: validatePassword,
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _isObscureText ? Icons.visibility_off : Icons.visibility,
+                ),
+                color: Colors.grey,
+                onPressed: _toggleObscureText,
+              ),
+            ),
+            verticalSpace(10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Forgot Password?',
+                style: TextStyles.lightGreyAlegreyaSans18Regular,
+              ),
+            ),
+            verticalSpace(40),
+            BlocBuilder<LoginCubit, LoginState>(
+              builder: (context, state) {
+                return AppTextButton(
+                  buttonText: state.maybeWhen(
+                    loading: () => 'Logging In...',
+                    orElse: () => 'Login',
+                  ),
+                  textStyle: TextStyles.whiteAlegreyaSans25Medium,
+                  onPressed: state.maybeWhen(
+                    loading: () => () {},
+                    orElse: () => _login,
+                  ),
+                );
+              },
+            ),
+            verticalSpace(16),
+          ],
         ),
-        verticalSpace(10),
-        Align(
-          alignment: Alignment.centerRight,
-          child: Text(
-            'Forgot Password?',
-            style: TextStyles.lightGreyAlegreyaSans18Regular,
-          ),
-        ),
-        verticalSpace(40),
-        AppTextButton(
-          buttonText: 'Login',
-          textStyle: TextStyles.whiteAlegreyaSans25Medium,
-          onPressed: () {},
-        ),
-        verticalSpace(16),
-      ],
+      ),
     );
   }
 }
