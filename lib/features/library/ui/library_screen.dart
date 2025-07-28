d20c09392e2082aad2d3e4ac5be6ca5424ba1d97
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-07-28 03:44:50 +0300

    refactor: update layout and styling for LibraryScreen and TrackContainer components

--- lib/features/library/ui/library_screen.dart
+++ lib/features/library/ui/library_screen.dart
@@ -6,22 +6,18 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:soulsync/features/library/ui/widgets/track_item_row.dart';
 import 'package:soulsync/features/library/ui/widgets/track_container.dart';
 
-
 class LibraryScreen extends StatelessWidget {
   const LibraryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
             // App Top Bar
-            SliverToBoxAdapter(
-              child: const AppTopBar(),
-            ),
-            
+            SliverToBoxAdapter(child: const AppTopBar()),
+
             // Featured Track Container
             SliverToBoxAdapter(
               child: Padding(
@@ -35,7 +31,7 @@ class LibraryScreen extends StatelessWidget {
                 ),
               ),
             ),
-            
+
             // Tracks List
             SliverPadding(
               padding: EdgeInsets.symmetric(horizontal: 20.w),
@@ -48,11 +44,9 @@ class LibraryScreen extends StatelessWidget {
                 ),
               ),
             ),
-            
+
             // Bottom spacing for navigation bar
-            SliverToBoxAdapter(
-              child: verticalSpace(100),
-            ),
+            SliverToBoxAdapter(child: verticalSpace(100)),
           ],
         ),
       ),
@@ -65,30 +59,30 @@ class LibraryScreen extends StatelessWidget {
         'title': 'Painting Forest',
         'listeners': '59899 Listening',
         'duration': '20 Min',
-        'image': 'assets/images/painting_forest.png',
+        'image': 'assets/images/track_rectangle.png',
       },
       {
         'title': 'Mountaineers',
         'listeners': '45679 Listening',
         'duration': '15 Min',
-        'image': 'assets/images/mountaineers.png',
+        'image': 'assets/images/track_rectangle.png',
       },
       {
         'title': 'Lovely Deserts',
         'listeners': '9452 Listening',
         'duration': '39 Min',
-        'image': 'assets/images/lovely_deserts.png',
+        'image': 'assets/images/track_rectangle.png',
       },
       {
         'title': 'The Hill Sides',
         'listeners': '56897 Listening',
         'duration': '50 Min',
-        'image': 'assets/images/hill_sides.png',
+        'image': 'assets/images/track_rectangle.png',
       },
     ];
 
     final track = tracks[index];
-    
+
     return TrackItemRow(
       title: track['title']!,
       listeners: track['listeners']!,
--- lib/features/library/ui/widgets/track_container.dart
+++ lib/features/library/ui/widgets/track_container.dart
@@ -10,8 +10,8 @@ class TrackContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 223.w,
-      height: 387.68.h,
+      width: double.infinity,
+      height: 250.h,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.r),
         image: const DecorationImage(
@@ -20,24 +20,24 @@ class TrackContainer extends StatelessWidget {
         ),
       ),
       child: Padding(
-        padding: EdgeInsets.only(left: 16.w, top: 20.h),
+        padding: EdgeInsets.all(24.w),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            verticalSpace(15),
             Text("Relax Sounds", style: TextStyles.whiteAlegreya32Medium),
-            verticalSpace(8),
+            verticalSpace(12),
             SizedBox(
-              width: 160.w,
+              width: 200.w,
               child: Text(
                 "Sometimes the most productive thing you can do is relax.",
                 style: TextStyles.whiteAlegreyaSans15Medium,
               ),
             ),
-            const Spacer(),
+            verticalSpace(27),
             Container(
               width: 157.82.w,
               height: 44.6.h,
-              margin: EdgeInsets.only(bottom: 16.h),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10.r),
@@ -45,23 +45,20 @@ class TrackContainer extends StatelessWidget {
               child: InkWell(
                 borderRadius: BorderRadius.circular(10.r),
                 onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 12.w),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "play now",
-                        style: TextStyles.blackAlegreyaSans15Medium,
-                      ),
-                      horizontalSpace(8),
-                      SvgPicture.asset(
-                        'assets/svgs/black_play_icon.svg',
-                        width: 14.w,
-                        height: 14.h,
-                      ),
-                    ],
-                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "play now",
+                      style: TextStyles.blackAlegreyaSans15Medium,
+                    ),
+                    horizontalSpace(8),
+                    SvgPicture.asset(
+                      'assets/svgs/black_play_icon.svg',
+                      width: 16.w,
+                      height: 16.h,
+                    ),
+                  ],
                 ),
               ),
             ),
